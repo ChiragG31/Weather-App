a292f0bf7274ffa09e1486c27011a1fc5dddd324
ChiragG31 <gchirag29gmail.com>
2023-07-24 20:22:41 +0530

    First commit

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5fe400c878msh4dbef9b193eb955p19a915jsnb67952afca1c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "662057607789c9de6b0500ee9ea87bd7";
